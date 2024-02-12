249e2d26fb84c2f0e852ee054c7b38b3662d999e
OuterScripts <157838073+OuterScripts@users.noreply.github.com>
2024-02-12 09:29:11 -0600

    ae

--- RandomThingsOrDev/clipboardspammer.lua
+++ RandomThingsOrDev/clipboardspammer.lua
@@ -9,42 +9,39 @@ local Tab = Window:MakeTab({
 	PremiumOnly = false
 })
 
-task.spawn(function()
-    repeat
-        task.wait()
-        setclipboard("apple")
-        setclipboard("blanked is cool")
-        setclipboard("champion is weird")
-        setclipboard("delta")            
-        setclipboard("end function")
-        setclipboard("fluxus")
-        setclipboard("green")
-        setclipboard("hi")
-        setclipboard("itch")
-        setclipboard("just kidding")
-        setclipboard("keep")
-        setclipboard("lamp")
-        setclipboard("month")
-        setclipboard("nuts")
-        setclipboard("outer")
-        setclipboard("punk team")
-        setclipboard("queen")
-        setclipboard("run")
-        setclipboard("sammz")
-        setclipboard("trenches")
-        setclipboard("undo")
-        setclipboard("venus")
-        setclipboard("when")
-        setclipboard("xenon")
-        setclipboard("youve")
-        setclipboard("zen")
-    end
-end)
-
 Tab:AddToggle({
-	Name = "Spam",
+	Name = "Spammer",
 	Default = false,
-	Callback = function(Value)
-		print(Value)
-	end    
+	Callback = function(callback)
+        if callback then
+            repeat
+                setclipboard("apple")
+                setclipboard("blanked is cool")
+                setclipboard("champion is weird")
+                setclipboard("delta")            
+                setclipboard("end function")
+                setclipboard("fluxus")
+                setclipboard("green")
+                setclipboard("hi")
+                setclipboard("itch")
+                setclipboard("just kidding")
+                setclipboard("keep")
+                setclipboard("lamp")
+                setclipboard("month")
+                setclipboard("nuts")
+                setclipboard("outer")
+                setclipboard("punk team")
+                setclipboard("queen")
+                setclipboard("run")
+                setclipboard("sammz")
+                setclipboard("trenches")
+                setclipboard("undo")
+                setclipboard("venus")
+                setclipboard("when")
+                setclipboard("xenon")
+                setclipboard("youve")
+                setclipboard("zen")
+            end
+        end 
+    end   
 })
